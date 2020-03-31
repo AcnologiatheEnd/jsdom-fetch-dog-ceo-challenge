ea6c6489c87f86b73f68b496ac3950082c8003b5
Winston  Chung <18.chung.winston@ctkprep.org>
2020-03-31 20:39:00 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
   const breedUrl = 'https://dog.ceo/api/breeds/list/all'
   var breeds; //declare breeds as a global variable so you can access it when calling it within arguments of functions that are called back wtihin other functions like triggerFilter() (read about hoisting)
@@ -6,9 +5,9 @@ const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 function fetchDogs() {
     fetch(imgUrl)
    .then(resp => resp.json())
-   .then(renderDogs); //note that you don't even have to use argument or specify that should be called on the json converison of the response, implicit in named fucntion renderDogs - to be explicit follow pattern in fetchBreeds
+   .then(renderDogs); 
 }
-
+//handles our image display
 function renderDogs(json) {
   const main = document.getElementById('dog-image-container')
   const imageArray = json.message;
@@ -24,24 +23,66 @@ function fetchBreeds(){
  .then(resp => resp.json())
  .then(json => {
   breeds = json.message;
-   renderBreeds(breeds);
   });
 }
 
-function renderBreeds(breeds){
-  const ul = document.getElementById('dog-breeds')
-  //if you don't remove the li child elements then the search results will get appended to the full list that was renedered when renderbreeds called upon Dom content load
-  while (ul.firstChild) {
-    ul.firstChild.remove();
+
+ 
+
+ // listens for the letter change then pass the letter to the findMatch funcion along with breeds containing all the dog breeds
+function triggerFilter(){
+    let breedDropdown = document.getElementById('breed-dropdown')
+    breedDropdown.addEventListener('change', function (event){
+    let letterToSearch = breedDropdown.value;
+    findMatch(letterToSearch, breeds)
+  });
+  }
+//looks for all the breeds in the breed var then appends them to the searchResult object to be passed to renderBreeds function
+  function findMatch(letterToSearch, breeds){
+    const strg = String(letterToSearch)
+    const searchResult = {}
+    for (const key in breeds) {
+      if (key.startsWith(strg)) { searchResult[key] = breeds[key];}
     }
-    
-    //you have to use for..in loop as opposed to forEach because the value of message in this example is a nested object and forEach only works with arrays. Also with for..in you only get access to index/keys of the object, which is fine here because the data we want is stored in the keys
-    for (const breed in breeds) {
-      const li = document.createElement("li");
-      li.innerText = breed;
-      console.log(breed);
-      ul.append(li);
-      li.addEventListener('click', changeColor);
+    renderBreeds(searchResult);
+    console.log(searchResult)
+  }
+//changes the color of the list items upon click
+  function changeColor(e){
+    e.target.classList.add('clicked');
+  }
+// creates and handles our list of breeds based on the data gathered from triggerFilter and FindMatch functions
+  function renderBreeds(data){
+
+    const ul = document.getElementById('dog-breeds')
+    while (ul.firstChild) {
+     ul.firstChild.remove();
+     }
+ 
+    for (let key in data) {
+      let li = document.createElement("li");
+      if (data[key].length >= 1) {
+        li.innerText = key
+        ul.appendChild(li)
+        let ul2 =  document.createElement("ul");
+         data[key].forEach(function(name){
+          let li2 = document.createElement("li");
+          li2.innerText = name;
+          ul2.append(li2);
+          li.append(ul2);
+        });
+ 
+      } else {
+        li.innerText = key;
+      }
+ 
+      ul.appendChild(li);
+    li.addEventListener('click', changeColor); //note you don't need argument or () to invoke function because it is described explicitly below
+    }
   }
 
-}
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchBreeds();
+    fetchDogs();
+    triggerFilter();
+  });
